bbbbc657775af7417a037836ebd33dfbeca88b10
runishiwo <66987131+runishiwo@users.noreply.github.com>
2021-10-20 18:26:16 +0800

    Update del.sh

--- del.sh
+++ del.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sed -i 's/helpu.cf/www.baidu.com/g' *.js
-sed -i 's/transfer.nz.lu/www.baidu.com/g' *.js
-sed -i 's/jdsharecode.xyz/www.baidu.com/g' *.js
-sed -i 's/smiek.tk/www.baidu.com/g' *.js
+  sed -i 's/helpu.cf/www.baidu.com/g' *.js
+  
+  sed -i 's/transfer.nz.lu/www.baidu.com/g' *.js
+  
+  sed -i 's/jdsharecode.xyz/www.baidu.com/g' *.js
+  
+  sed -i 's/smiek.tk/www.baidu.com/g' *.js
